63f1301dd6d2d4760c09cff80628ab727563188f
Lenur <abdiramanovlenur@gmail.com>
2026-09-03 12:17:14 +0500

    update

--- src/entities/companies/api/endpoints.ts
+++ src/entities/companies/api/endpoints.ts
@@ -1,5 +1,5 @@
 export const endpoints = {
   LIST: '/api/admin/company',
   BY_ID: '/api/admin/company/details',
-  MANUAL_LIST: '/company/unowned'
+  MANUAL_LIST: '/api/mano/company/unowned'
 } as const;
